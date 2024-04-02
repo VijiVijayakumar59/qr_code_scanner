f75e7e6e610caec3c4367100afa1a95f369047cb
VijiVijayakumar59 <vv964827@gmail.com>
2024-04-02 23:44:27 +0530

    code updated

--- lib/presentation/scan_qr.dart
+++ lib/presentation/scan_qr.dart
@@ -4,7 +4,7 @@ import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:qrcode/presentation/success_screen.dart';
 
 class QRViewExample extends StatefulWidget {
-  const QRViewExample({Key? key}) : super(key: key);
+  const QRViewExample({super.key});
 
   @override
   State<StatefulWidget> createState() => _QRViewExampleState();
